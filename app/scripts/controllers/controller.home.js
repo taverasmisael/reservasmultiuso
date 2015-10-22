c4d24c8a77e3da8540899bcb2f420d10634d653e
TaverasMisael <macrot29@gmail.com>
2015-10-22 09:56:48 -0400

    Markup For `main.tpl` view with dummy data
    Se crea el marcado basico para la vista principal de reservaciones con
    datos falsos ("dummy data") para tener una idea de como se vera mas
    adelante

--- app/scripts/controllers/controller.home.js
+++ app/scripts/controllers/controller.home.js
@@ -6,10 +6,50 @@
     HomeController.$inject = [];
 
     function HomeController () {
+      var vm = this;
+      vm.todayDate = new Date();
+      vm.reservaciones = {};
+
       active();
 
       function active () {
         console.log('Active Main...');
+        vm.reservaciones.today = [
+          {
+            who: 'SpiderMan',
+            what: 'Seccion 0423',
+            date: {
+              starts: new Date(),
+              ends: new Date()
+            }
+          },
+          {
+            who: 'Batman',
+            what: 'Seccion 0325',
+            date: {
+              starts: new Date(),
+              ends: new Date()
+            }
+          }
+        ];
+        vm.reservaciones.tomorrow = [
+          {
+            who: 'SuperMan',
+            what: 'Seccion 0423',
+            date: {
+              starts: new Date(),
+              ends: new Date()
+            }
+          },
+          {
+            who: 'IronMan',
+            what: 'Seccion 0325',
+            date: {
+              starts: new Date(),
+              ends: new Date()
+            }
+          }
+        ];
       }
     }
 })();
