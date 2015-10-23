ca57294eb515d1695800247d1a02114d8c5d12e3
TaverasMisael <macrot29@gmail.com>
2015-10-23 16:11:34 -0400

    Complete Dummy SearchView. Add `Fixes`. Create Dummy Controller functs
    Completa el modelo o maqueta de la vista de busqueda desde la vista mas
    basica.
    
    Agrega el archivo que habiamos hablado de `fixes` donde se encontaran
    algunos arreglos para la visualizacion correcta de ciertas partes o
    modulos de nuestra aplicacion.
    
    El controlador tambien se le ha agregado funcionalidad muy basica para
    filtrar data y devolverla.

--- app/scripts/controllers/controller.search.js
+++ app/scripts/controllers/controller.search.js
@@ -5,10 +5,59 @@
 
     SearchController.$inject = [];
     function SearchController () {
+        var vm = this;
+        vm.querySearch = querySearch;
         active();
+        var monthList = Array.apply(0, Array(12)).map(
+            function(_,i){
+              return moment().month(i).format('MMMM')
+            }
+        );
+        vm.months = [];
+        vm.profesorsList = loadAll();
+        for (var i = 0; i < monthList.length; i += 1) {
+          var currentMonth = {
+            index: i,
+            name: monthList[i]
+          };
+        }
 
         function active () {
           console.log('Active Search....');
+          vm.today = new Date();
         }
+
+
+        function querySearch (query) {
+          console.log('Buscando...');
+          var results = query ? vm.profesorsList.filter( createFilterFor(query) ) : [];
+          return results;
+        }
+
+        // TEMPORARY FUNCTIONS
+        function createFilterFor(query) {
+             var lowercaseQuery = angular.lowercase(query);
+             return function filterFn(profesor) {
+                console.log('Filtrando...');
+               return (profesor.value.indexOf(lowercaseQuery) === 0);
+             };
+           }
+
+
+           function loadAll() {
+                 var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+                         Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+                         Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+                         Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+                         North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+                         South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+                         Wisconsin, Wyoming';
+                 return allStates.split(/, +/g).map( function (state) {
+                   return {
+                     value: state.toLowerCase(),
+                     name: state
+                   };
+                 });
+               }
     }
 })();
