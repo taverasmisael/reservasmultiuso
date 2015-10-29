394da90aab88560ad373332f16ac10610f142ad0
TaverasMisael <macrot29@gmail.com>
2015-10-29 10:16:07 -0400

    Add md-calendar styles. Add mindate for reservations

--- app/scripts/controllers/controller.admin.js
+++ app/scripts/controllers/controller.admin.js
@@ -11,6 +11,8 @@
         function active () {
           console.log('Administrating...');
           vm.today = new Date();
+          vm.minReservationDate = new Date(vm.today.getFullYear(), vm.today.getMonth(), vm.today.getDate() - 1);
+          console.log(vm.minReservationDate);
         }
     }
 })();
