1d40cf67a63f2e4dfa70eefea67c1a596b835b0f
TaverasMisael <macrot29@gmail.com>
2015-10-29 16:45:35 -0400

    Initialize Reservaciones Service Mockup

--- app/scripts/services/service.reservas.js
+++ app/scripts/services/service.reservas.js
@@ -1,4 +1,40 @@
 (function(){
     'use strict';
-    // Main service for retriving info from backend Service
+    angular.module('reservacionesMulti')
+            .service('Reservaciones', Reservaciones);
+
+    Reservaciones.$inject = ['$q', '$firebaseObject', '$firebaseArray', 'FURL'];
+    function Reservaciones ($q, $firebaseObject, $firebaseArray, FURL) {
+      var ref = new Firebase(FURL),
+          reservaciones = $firebaseArray(ref.child('reservaciones'));
+        var ReservacionesService = {
+          all: reservaciones,
+          today: getToday,
+          findById: findById,
+          create: create,
+          remove: remove,
+          isActive: isActive
+        };
+        return ReservacionesService;
+
+        function getToday () {
+          // Function for Getting all Reservations, only for today
+        }
+
+        function findById () {
+          // Function to find just one Reservation
+        }
+
+        function create () {
+          // Function For Adding New Reservations
+        }
+
+        function remove () {
+          // Function for removing an especific REservation
+        }
+
+        function isActive () {
+          // Function for consulting if a Reservention is actually active
+        }
+    }
 })();
