9453110b3662a5d3844e8b1a08ac01d6326d46eb
TaverasMisael <macrot29@gmail.com>
2015-12-10 11:37:24 -0400

    Start Implementation for EditProfile

--- app/scripts/controllers/controller.users.js
+++ app/scripts/controllers/controller.users.js
@@ -3,14 +3,23 @@
     angular.module('reservacionesMulti')
             .controller('UsersController', UsersController);
 
-    UsersController.$inject = ['profiles'];
-    function UsersController (profiles) {
+    UsersController.$inject = ['$mdToast', 'profiles'];
+    function UsersController ($mdToast, profiles) {
         var vm = this;
+        vm.editProfile = editProfile;
 
         active();
 
         function active () {
           vm.profiles = profiles;
+        }
+
+        function editProfile (id) {
+          $mdToast.show(
+            $mdToast.simple()
+            .content(id)
+            .position('right bottom')
+          );
         }
     }
 })();
