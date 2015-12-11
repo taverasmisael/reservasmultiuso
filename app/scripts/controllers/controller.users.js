59befed401bfbc3b6144defa2e47080ff5c79b49
TaverasMisael <macrot29@gmail.com>
2015-12-11 10:33:26 -0400

    Create ExtendibleDialogObject. Call Dialog in Creation Mode
    El objecto `dialogOptions` almacena la configuracion compartida para
    abrir cuadros de dialogos, esta se extiende con jQuery cada vez que
    es llamada, por un objeto diferente.
    
    Se ha llamado al mismo Dialogo con opciones de creacion para que este
    pueda no solo editar usuario, sino tambien crearlos. Agregaremos la
    opcion de eliminarlos.

--- app/scripts/controllers/controller.users.js
+++ app/scripts/controllers/controller.users.js
@@ -1,49 +1,67 @@
-(function(){
+(function() {
     'use strict';
     angular.module('reservacionesMulti')
-            .controller('UsersController', UsersController);
+        .controller('UsersController', UsersController);
 
     UsersController.$inject = ['$mdMedia', '$mdToast', '$mdDialog', 'Auth', 'profiles'];
-    function UsersController ($mdMedia, $mdToast, $mdDialog, Auth, profiles) {
+
+    function UsersController($mdMedia, $mdToast, $mdDialog, Auth, profiles) {
         var vm = this;
+        var dialogOptions = {
+            controller: 'DialogController',
+            controllerAs: 'DialogCtrl',
+            templateUrl: 'views/dialogs/users.dlg.html',
+            clickOutsideToClose: false,
+            parent: angular.element(document.body)
+        };
         vm.editProfile = editProfile;
+        vm.createUser = createUser;
 
         active();
 
-        function active () {
-          vm.profiles = profiles;
+        function active() {
+            vm.profiles = profiles;
         }
 
-        function editProfile (event, id) {
-          Auth.getProfile(id).$loaded().then(function (response) {
-            $mdDialog.show({
-                controller: 'DialogController',
-                controllerAs: 'DialogCtrl',
-                templateUrl: 'views/dialogs/users.dlg.html',
-                targetEvent: event,
-                clickOutsideToClose: false,
-                locals: {
+        function editProfile(event, id) {
+            Auth.getProfile(id).$loaded().then(function(response) {
+                var editDialog = $.extend({}, dialogOptions);
+                editDialog.event = event;
+                editDialog.locals = {
                     currentUser: response,
                     state: 'editando'
-                },
-                parent: angular.element(document.body)
-            }).then(function(respuesta) {
-                $mdToast.show(
-                  $mdToast.simple()
-                  .content(respuesta)
-                  .position('right bottom')
-                );
+                };
+                $mdDialog.show(editDialog).then(_dialogComplete).catch(_dialogAbort);
             }).catch(function(err) {
-              $mdToast.show(
+                console.log(err);
+            });
+        }
+
+        function createUser (event) {
+          var createDialog = $.extend({}, dialogOptions);
+          createDialog.event = event;
+          createDialog.locals = {
+              currentUser: {},
+              state: 'creando'
+          };
+          $mdDialog.show(createDialog).then(_dialogComplete).catch(_dialogAbort);
+        }
+
+        function _dialogComplete(respuesta) {
+            $mdToast.show(
+                $mdToast.simple()
+                .content(respuesta)
+                .position('right bottom')
+            );
+        }
+
+        function _dialogAbort(err) {
+            $mdToast.show(
                 $mdToast.simple()
                 .content(err)
                 .position('right bottom')
-              );
-                console.log(err);
-            });
-          }).catch(function (err) {
+            );
             console.log(err);
-          });
         }
     }
 })();
